bf8618097892ac6aa7d82f9e118f3fd4293aa9c8
GigaHunter071119 <josearturocruznery@gmail.com>
2026-05-15 22:11:23 -0600

    guardado para cambio en repositorio

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-#------------------------------------------------------
-#   Función que muestra el menú de acciones (Arturo)
-#------------------------------------------------------
-
-def menu() -> int:
-
-    while True:
-        print("""
------ Seleccione una opción -----
-[1] Cargar informacion de CSV
-[2] Registrar participantes
-[3] Buscar participante
-[4] Modificar participante
-[5] Eliminar participante
-[6] Ver lista de participantes
-[7] Actualizar información de CSV
-[8] Serializar información a JSON
-[X] Terminar y salir
-----------------------------------
-""")
-        num = get_int("Opción")
-        if num == "X" or num == "x":
-            return num
-        elif 0 < num < 9:
-            return num
-        print("Opción no válida")
-        
-# --------------------------------------------------------------------------
-# Modulo que ejecuta las funciones en base a una funcion de menu (Arturo)
-# --------------------------------------------------------------------------
-
 from funciones import *
 
 participantes = list()
